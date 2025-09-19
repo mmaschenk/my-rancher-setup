b7ce39ee711acebe0065ff4307d01c20fdc44865
Mark Schenk <m.m.a.schenk@tudelft.nl>
2025-09-19 09:54:52 +0200

    Cluster file is parameter

--- create-rke2-cluster.sh
+++ create-rke2-cluster.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 TOKEN="$1"
+TEMPLATEFILE="${2:-rke2-cluster-calico.yaml}"
+
+if [ -z "$TOKEN" ]; then
+  echo "Usage: $0 <Rancher API Bearer Token> [<Cluster Template File>]"
+  echo "If <Cluster Template File> is not provided, 'rke2-cluster-calico.yaml' will be used by default."
+  exit 1
+fi
 
 rancher  login --skip-verify --token "$TOKEN" https://localhost
 
-rancher kubectl apply -f rke2-cluster-calico.yaml
+rancher kubectl apply -f "$TEMPLATEFILE"
 rancher cluster list
 
 echo "Cluster creation initiated. Go to cluster management, select the main cluster and follow the instructions to add nodes."
